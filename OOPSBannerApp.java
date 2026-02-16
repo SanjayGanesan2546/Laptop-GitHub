49ea9db77f8293260f7cc8a3d97774de7b882ccd
Sanjay2546 <sanjayganesan12@gmail.com>
2026-02-16 19:26:25 +0530

    UC2: Print large ASCII banner of OOPS using manual print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,20 @@
+/**
+ * OOPSBannerApp
+ * Prints large ASCII banner of "OOPS".
+ *
+ * @author Sanjay
+ * @version 2.0
+ */
 public class OOPSBannerApp {
-public static void main(String[] args) {
-System.out.println("   ***   " + " "+"    ***   " + " " + "  ******    "+" "+"   *****");
-System.out.println(" **   **" + " "+"   **   **" + " " + "   **    ** "+" "+"  **");
-System.out.println("**     **" + " "+" **     **" + " " + "  **     **"+" "+" **");
-System.out.println("**     **" + " "+" **     **" + " " + "  **    **"+" "+"   **");
-System.out.println("**     **" + " "+" **     **" + " " + "  ******   "+" "+"    ***");
-System.out.println("**     **" + " "+" **     **" + " " + "  **    "+" "+"          **");
-System.out.println("**     **" + " "+" **     **" + " " + "  **    "+" "+"           **");
-System.out.println(" **   ** " + " "+"  **   **" + " " + "   **    "+" "+"          **");
-System.out.println("   ***   " + " "+"    ***   " + " " + "  **    "+" "+"     *****");
-}
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println(" *****   *****   *****   ***** ");
+    }
 }
